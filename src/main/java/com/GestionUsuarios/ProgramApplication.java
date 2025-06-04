4c6051c865f0240bd7e67c4ca51cc6394eab2332
MAOIRO17 <maoiro17@gmail.com>
2025-06-04 17:22:08 +0200

    Creación de la entidad Usuario y su respectivo repositorio

--- src/main/java/com/GestionUsuarios/ProgramApplication.java
+++ src/main/java/com/GestionUsuarios/ProgramApplication.java
@@ -1,4 +1,4 @@
-package com.program.program;
+package com.GestionUsuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/GestionUsuarios/model/Usuario.java
@@ -0,0 +1,23 @@
+package com.GestionUsuarios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String username;
+    private String password;
+    private String telefono;
+
+}
